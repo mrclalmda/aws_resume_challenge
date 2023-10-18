7c179587ac3d5abb7011fd07eaf5f89243572546
Marcelo Almeida <marcelo_almeida@live.com>
2023-10-17 23:39:55 -0300

    Provider

--- terraform/acm/main.tf
+++ terraform/acm/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = "mrclalmd.live"
   validation_method = "DNS"
